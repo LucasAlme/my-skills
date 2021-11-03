601aec56b628ef6df95f9a43152a5418ddaa4e2e
Lucas Almeida <lucasalmeidaprogramacao@gmail.com>
2021-11-03 18:08:08 -0300

    Adicionando mensagem de horario, e finalizando javascript

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import Button from '../../components/button';
 import style from './style';
@@ -7,7 +7,19 @@ export default function Home() {
 
     const [newSkill, setNewSkill] = useState('');
     const [mySkills, setMySkills] = useState([]);
+    const [gretting, setGretting] = useState('');
 
+    useEffect(() => {
+        const currentHour = new Date().getHours();
+
+        if(currentHour < 12){
+            setGretting('Good morning');
+        }else if(currentHour >= 12 && currentHour < 18){
+            setGretting('Good afternoon');
+        }else{
+            setGretting('Good night');
+        }
+    }, [])
 
     function handleNewSkill() {
         setMySkills(oldState => [...oldState, newSkill]);
@@ -16,6 +28,7 @@ export default function Home() {
     return (
         <View style={style.background}>
             <Text style={style.title}>Welcome, Lucas</Text>
+            <Text style={{ marginVertical: 5 }}>{gretting}</Text>
             <TextInput style={style.input} placeholder="New Skill" value={newSkill} onChangeText={(txt) => setNewSkill(txt)} />
             <Button customStyle={style.btnStyle} value="Add" onPress={handleNewSkill} opacity={0.7} />
             <Text style={[style.title, { marginVertical: 50 }]}>My Skills</Text>
